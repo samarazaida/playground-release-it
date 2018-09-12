5afc302e0f7103fc732597485c69ab2b29752481
samarazaida <samarazaida@gmail.com>
2018-09-12 03:50:12 -0300

    style: fix lint errors

--- src/main.js
+++ src/main.js
@@ -1,22 +1,14 @@
-const soma = (...args) =>
-  args.reduce((acc, cur) =>
-    acc + cur
-  , 0);
+const soma = (...args) => args.reduce((acc, cur) => acc + cur,
+  0);
 
-const subtracao = (...args) =>
-  args.reduce((acc, cur, index) =>
-    index === 0 ? acc : acc - cur
-  , args[0]);
+const subtracao = (...args) => args.reduce((acc, cur, index) => (index === 0 ? acc : acc - cur),
+  args[0]);
 
-const multiplicacao = (...args) =>
-  args.reduce((acc, cur, index) =>
-    index === 0 ? acc : acc * cur
-  , args[0]);
+const multiplicacao = (...args) => args.reduce((acc, cur, index) => (index === 0 ? acc : acc * cur),
+  args[0]);
 
-const divisao = (...args) =>
-  args.reduce((acc, cur, index) =>
-    index === 0 ? acc : acc / cur
-  , args[0]);
+const divisao = (...args) => args.reduce((acc, cur, index) => (index === 0 ? acc : acc / cur),
+  args[0]);
 
 export default {
   soma,
